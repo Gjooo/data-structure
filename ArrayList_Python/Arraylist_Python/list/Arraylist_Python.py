dd758fb181a38552b356dd4a234f50c0c18959be
Gjooo <kgjoo@soongsil.ac.kr>
2025-08-11 14:11:51 +0900

    주석 추가

--- ArrayList_Python/Arraylist_Python/list/Arraylist_Python.py
+++ ArrayList_Python/Arraylist_Python/list/Arraylist_Python.py
@@ -1,4 +1,4 @@
-from list.listNode import ListNode
+from listNode import ListNode
 
 class LinkedList:
     def __init__(self):
@@ -14,18 +14,39 @@ class LinkedList:
         return (self.__numItems == 0)
 
     def getSize(self):
+        """리스트의 크기를 반환하는 함수
+
+        Returns -> int:
+            리스트의 원소 개수
+        """
         return self.__numItems
 
     def clear(self):
+        """리스트를 비우는 함수
+        
+        Returns -> None:
+        """
         self.__head = ListNode("dummy", None)
         self.__numItems = 0
+        return None
 
     def append(self, newItem):
+        """리스트의 마지막에 새로운 원소를 추가하는 함수
+        Args:
+            newItem: 추가할 원소
+        Returns -> None:
+        """
         newNode = ListNode(newItem, self.__head)  # 새 노드를 생성
         self.__head.next = newNode  # 새 노드를 리스트의 첫 번째 노드로 설정
         self.__numItems += 1  # 리스트 크기 증가
 
     def insert(self, newItem, index):
+        """리스트의 특정 위치에 새로운 원소를 삽입하는 함수
+        Args:
+            newItem: 삽입할 원소
+            index: 삽입할 위치 (0부터 시작)
+        Returns -> None:
+        """
         if index < 0 or index > self.__numItems:
             raise IndexError("Index out of bounds")
         newNode = ListNode(newItem, None)
@@ -45,6 +66,12 @@ class LinkedList:
         return newItem
 
     def pop(self, index):
+        """리스트에서 특정 위치의 원소를 제거하고 반환하는 함수
+        Args:
+            index: 제거할 원소의 위치 (0부터 시작)
+        Returns -> item:
+            제거된 원소
+        """
         if index < 0 or index >= self.__numItems:
             raise IndexError("Index out of bounds")
         if index == 0:
@@ -62,6 +89,12 @@ class LinkedList:
         return item
 
     def remove(self, item):
+        """리스트에서 특정 원소를 제거하는 함수
+        Args:
+            item: 제거할 원소
+        Returns -> item:
+            제거된 원소
+        """
         current = self.__head.next
         previous = self.__head
         while current is not None:
@@ -74,6 +107,12 @@ class LinkedList:
         raise ValueError("Item not found in the list")
 
     def __getnode(self, index:int):
+        """리스트에서 특정 위치의 노드를 반환하는 함수
+        Args:
+            index: 노드를 찾을 위치 (0부터 시작)
+        Returns -> ListNode:
+            특정 위치의 노드
+        """
         if self.__isEmpty:
             return None
         current = self.__head.next
@@ -81,6 +120,12 @@ class LinkedList:
             current = current.next
 
     def get(self, index:int):
+        """리스트에서 특정 위치의 원소를 반환하는 함수
+        Args:
+            index: 원소를 찾을 위치 (0부터 시작)
+        Returns -> item:
+            특정 위치의 원소
+        """
         if self.isEmpty:
             return None
         if (index>=0 and index<=self.__numItems -1):
@@ -89,11 +134,16 @@ class LinkedList:
             return None
 
     def index(self, x):
+        """리스트에서 특정 원소의 인덱스를 반환하는 함수
+        Args:
+            x: 찾을 원소
+        Returns -> int:
+            원소의 인덱스 (0부터 시작), 원소가 없으면 None
+        """
         current = self.__head.next
         for i in range(self.__numItems):
             if current.item == x:
                 return i
             current = current.next
-
         return None
-    
+
